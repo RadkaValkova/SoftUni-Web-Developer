dd43b8318799c34eb2316d28d90baeb5d5dc8093
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-04-25 07:34:25 +0300

    Create 03_capitals.py

---
+++ Programming Advanced Python/Comprehensions exercise/03_capitals.py
@@ -0,0 +1,4 @@
+# countries = ['Bulgaria', 'Romania', 'Germany', 'England']
+# capitals = ['Sofia', 'Bucharest', 'Berlin', 'London']
+
+[print(f'{el[0]} -> {el[1]}') for el in zip(input().split(', '),input().split(', '))]
