f69dab6137237a1a30d5354f8f23caafaadbfe44
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-04-26 20:38:34 +0300

    Create 04_operate.py

---
+++ Programming Advanced Python/Function_advanced/04_operate.py
@@ -0,0 +1,18 @@
+from functools import reduce
+
+
+def operate(operator, *args):
+    result = ''
+    if operator == '+':
+        result = reduce(lambda x,y: x+y, args)
+    elif operator == '-':
+        result = reduce(lambda x, y: x - y, args)
+    elif operator == '*':
+        result = reduce(lambda x, y: x * y, args)
+    elif operator == '/':
+        result = reduce(lambda x, y: x / y, args)
+    return result
+
+
+print(operate("-", 1, 2, 3))
+print(operate("+", 1, 2, 3))
