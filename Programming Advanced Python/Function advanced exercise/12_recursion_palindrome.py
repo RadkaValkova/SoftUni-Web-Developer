102da1ebea8f5d68c1e3e918f739ed11afc91fd5
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-04-29 07:04:57 +0300

    Create 12_recursion_palindrome.py

---
+++ Programming Advanced Python/Function advanced exercise/12_recursion_palindrome.py
@@ -0,0 +1,10 @@
+def palindrome(word, index):
+    if index == len(word) // 2:
+        return f'{word} is a palindrome'
+    if word[index] != word[len(word) -1 - index]:
+        return f'{word} is not a palindrome'
+    return palindrome(word, index + 1)
+
+
+print(palindrome("peter", 0))
+# print(palindrome("abcba", 0))
