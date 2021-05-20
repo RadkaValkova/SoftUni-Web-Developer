982adb44a13710a45f0a30e06a455cac0824c59b
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-05-20 10:39:01 +0300

    Create 02_email_validator.py

---
+++ Programming OOP Python/Encapsulation/02_email_validator.py
@@ -0,0 +1,37 @@
+class EmailValidator:
+    def __init__(self, min_length, mails, domains):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def validate(self, mail):
+        name, mail, domain = self.get_elements(self,mail)
+        return EmailValidator.__validate_name(self,name) \
+               and EmailValidator.__validate_mail(self, mail) \
+               and EmailValidator.__validate_domain(self,domain)
+
+    @staticmethod
+    def get_elements(self,mail):
+        name,others = mail.split('@')
+        mail,domain = others.split('.', 1)
+        return name, mail,domain
+
+    def __validate_name(self, name):
+        return len(name) >= self.min_length
+
+    def __validate_mail(self, mail):
+        return mail in self.mails
+
+    def __validate_domain(self, domain):
+        return domain in self.domains
+
+
+mails = ["gmail", "softuni"]
+domains = ["com", "bg"]
+email_validator = EmailValidator(6, mails, domains)
+print(email_validator.validate("pe77er@gmail.com"))
+print(email_validator.validate("georgios@gmail.net"))
+print(email_validator.validate("stamatito@abv.net"))
+print(email_validator.validate("abv@softuni.bg"))
+
+
