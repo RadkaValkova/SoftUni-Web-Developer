76e05ecbe29a7ef5b70697900070fa9597189ffb
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-05-21 07:06:01 +0300

    Create 02_multiple_inheritance.py

---
+++ Programming OOP Python/Inheritance/02_multiple_inheritance.py
@@ -0,0 +1,13 @@
+class Person:
+    def sleep(self):
+        return 'sleeping...'
+
+
+class Employee:
+    def get_fired(self):
+        return 'fired...'
+
+
+class Teacher(Person, Employee):
+    def teach(self):
+        return 'teaching...'
