687596ad59e85e6dade0a7bfdd7c30e5c67bb481
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-05-21 07:05:59 +0300

    Create 01_single_inheritance.py

---
+++ Programming OOP Python/Inheritance/01_single_inheritance.py
@@ -0,0 +1,16 @@
+class Animal:
+    def eat(self):
+        return 'eating...'
+
+
+class Dog(Animal):
+    def bark(self):
+        return 'barking...'
+
+
+# a = Animal()
+# d = Dog()
+# print(a.eat())
+# print(d.eat())
+# print(d.bark())
+
