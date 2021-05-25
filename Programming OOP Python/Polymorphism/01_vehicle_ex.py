f78e0ba8f49ef38de251500ff2bd8f7ace62da98
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-05-25 10:13:05 +0300

    Create 01_vehicle_ex.py

---
+++ Programming OOP Python/Polymorphism/01_vehicle_ex.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+
+
+class Vehicle(ABC):
+
+    @abstractmethod
+    def drive(self, distance):
+        pass
+
+    @abstractmethod
+    def refuel(self, fuel):
+        pass
+
+
+class Car(Vehicle):
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    def drive(self, distance):
+        summer_consumption = self.fuel_consumption + 0.9
+        if distance * summer_consumption <= self.fuel_quantity:
+            self.fuel_quantity -= distance * summer_consumption
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel
+
+
+class Truck(Vehicle):
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    def drive(self, distance):
+        summer_consumption = self.fuel_consumption + 1.6
+        if distance * summer_consumption <= self.fuel_quantity:
+            self.fuel_quantity -= distance * summer_consumption
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel * 0.95
+
+
+car = Car(20, 5)
+car.drive(3)
+print(car.fuel_quantity)
+car.refuel(10)
+print(car.fuel_quantity)
+truck = Truck(100, 15)
+truck.drive(5)
+print(truck.fuel_quantity)
+truck.refuel(50)
+print(truck.fuel_quantity)
