be5937a2f8ba873a56365a180f33208ec58338a6
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-06-05 06:28:18 +0300

    Create subscription.py

---
+++ Programming OOP Python/OOP Exercise/Atributes_and_methods/gym_04/project/subscription.py
@@ -0,0 +1,17 @@
+class Subscription:
+    id_counter = 1
+
+    def __init__(self, date:str, customer_id: int, trainer_id: int, exercise_id: int):
+        self.date = date
+        self.customer_id = customer_id
+        self.trainer_id = trainer_id
+        self.exercise_id = exercise_id
+        self.id = Subscription.id_counter
+        Subscription.id_counter += 1
+
+    @staticmethod
+    def get_next_id():
+        return Subscription.id_counter
+
+    def __repr__(self):
+        return f'Subscription <{self.id}> on {self.date}'
