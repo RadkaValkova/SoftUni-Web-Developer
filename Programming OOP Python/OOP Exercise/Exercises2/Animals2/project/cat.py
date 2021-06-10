23a60afcf1c5d200fefa5dd32da6b7e4c30b04fe
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-06-10 09:25:40 +0300

    Create cat.py

---
+++ Programming OOP Python/OOP Exercise/Exercises2/Animals2/project/cat.py
@@ -0,0 +1,14 @@
+# from Exercises2.Animals2.project.animal import Animal
+from project.animal import Animal
+
+
+class Cat(Animal):
+    def __init__(self, name, age, gender):
+        super().__init__(name, age, gender)
+
+    def __repr__(self):
+        return f'This is {self.name}. {self.name} is a {self.age} year old {self.gender} {self.__class__.__name__}'
+
+    def make_sound(self):
+        return 'Meow meow!'
+
