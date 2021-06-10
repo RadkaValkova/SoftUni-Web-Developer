b40234417bb467adbf10c50dde37f301d80536ee
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-06-10 09:25:46 +0300

    Create kitten.py

---
+++ Programming OOP Python/OOP Exercise/Exercises2/Animals2/project/kitten.py
@@ -0,0 +1,12 @@
+# from Exercises2.Animals2.project.cat import Cat
+from project.cat import Cat
+
+
+class Kitten(Cat):
+    def __init__(self, name, age):
+        super().__init__(name, age, gender='Female')
+
+    def make_sound(self):
+        return 'Meow'
+
+
