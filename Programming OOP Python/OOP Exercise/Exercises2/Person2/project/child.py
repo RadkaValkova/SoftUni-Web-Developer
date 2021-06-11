713f41aa8ff43d19d69ed18d5cb38205e144038f
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-06-11 10:24:15 +0300

    Create child.py

---
+++ Programming OOP Python/OOP Exercise/Exercises2/Person2/project/child.py
@@ -0,0 +1,10 @@
+# from Exercises2.Person2.project.person import Person
+from project.person import Person
+
+
+class Child(Person):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+
+
