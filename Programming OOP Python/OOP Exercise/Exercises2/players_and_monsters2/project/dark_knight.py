8b17f40535b91bfe997d00553ffa7e1dcd8f522f
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-06-12 07:11:51 +0300

    Create dark_knight.py

---
+++ Programming OOP Python/OOP Exercise/Exercises2/players_and_monsters2/project/dark_knight.py
@@ -0,0 +1,7 @@
+# from Exercises2.players_and_monsters2.project.knight import Knight
+from project.knight import Knight
+
+
+class DarkKnight(Knight):
+    def __init__(self, username, level):
+        super().__init__(username, level)
