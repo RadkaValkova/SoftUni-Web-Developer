f4489636821354a14d96fe0586dbd12b50cf702c
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-06-12 07:12:13 +0300

    Create soul_master.py

---
+++ Programming OOP Python/OOP Exercise/Exercises2/players_and_monsters2/project/soul_master.py
@@ -0,0 +1,7 @@
+# from Exercises2.players_and_monsters2.project.dark_wizard import DarkWizard
+from project.dark_wizard import DarkWizard
+
+
+class SoulMaster(DarkWizard):
+    def __init__(self, username, level):
+        super().__init__(username, level)
