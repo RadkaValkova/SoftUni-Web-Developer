8b15a194c034e483536b16fe9c84ffaa2329c6f4
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-06-14 11:20:37 +0300

    Create vehicle2.py

---
+++ Programming OOP Python/OOP Exercise/Exercises2/vehicle2.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    @abstractmethod
+    def drive(self,distance):
+        pass
+
+    @abstractmethod
+    def refuel(self, fuel):
+        pass
+
+
+class Car(Vehicle):
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    def drive(self, distance):
+        summer_consumption = self.fuel_consumption + 0.9
+        fuel_needed = distance * summer_consumption
+        if self.fuel_quantity >= fuel_needed:
+            self.fuel_quantity -= fuel_needed
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel
+
+
+class Truck(Vehicle):
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    def drive(self, distance):
+        summer_consumption = self.fuel_consumption + 1.6
+        fuel_needed = distance * summer_consumption
+        if self.fuel_quantity >= fuel_needed:
+            self.fuel_quantity -= fuel_needed
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel * 0.95
+
+
+car = Car(20, 5)
+car.drive(3)
+print(car.fuel_quantity)
+car.refuel(10)
+print(car.fuel_quantity)
+truck = Truck(100, 15)
+truck.drive(5)
+print(truck.fuel_quantity)
+truck.refuel(50)
+print(truck.fuel_quantity)
+
+
