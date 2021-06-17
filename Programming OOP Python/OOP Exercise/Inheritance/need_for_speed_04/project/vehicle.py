0b1edac95c27dda06e490d7f6bc88f8f3bf4311e
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-06-17 06:30:05 +0300

    Create vehicle.py

---
+++ Programming OOP Python/OOP Exercise/Inheritance/need_for_speed_04/project/vehicle.py
@@ -0,0 +1,15 @@
+
+class Vehicle:
+    DEFAULT_FUEL_CONSUMPTION = 1.25
+
+    def __init__(self, fuel, horse_power):
+        self.fuel = fuel
+        self.horse_power = horse_power
+        self.fuel_consumption = Vehicle.DEFAULT_FUEL_CONSUMPTION
+
+    def drive(self, kilometers):
+        fuel_needed = self.fuel_consumption * kilometers
+        if fuel_needed <= self.fuel:
+            self.fuel -= fuel_needed
+
+
