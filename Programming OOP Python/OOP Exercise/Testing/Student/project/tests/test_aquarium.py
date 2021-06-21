b77d966366dd12cadfdc0cb46174b56cb1b30eab
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-06-21 06:26:36 +0300

    Create test_aquarium.py

---
+++ Programming OOP Python/OOP Exercise/Testing/Student/project/tests/test_aquarium.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+
+
+class TestAquarium(TestCase):
+    def test_feed(self):
+        self.fail()
+
+
+class TestAquarium(TestCase):
+    def test_comfort(self):
+        self.fail()
+
+    def test_name(self):
+        self.fail()
+
+    def test_name(self):
+        self.fail()
+
+    def test_add_fish(self):
+        self.fail()
+
+    def test_remove_fish(self):
+        self.fail()
+
+    def test_add_decoration(self):
+        self.fail()
+
+    def test_feed(self):
+        self.fail()
+
+    def test_get_info(self):
+        self.fail()
