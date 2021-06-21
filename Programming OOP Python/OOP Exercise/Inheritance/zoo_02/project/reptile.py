c6fd0883b94f84c5d3e276461f75445ee07056d9
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-06-21 06:21:04 +0300

    Create reptile.py

---
+++ Programming OOP Python/OOP Exercise/Inheritance/zoo_02/project/reptile.py
@@ -0,0 +1,8 @@
+# from Inheritance.zoo_02.project.animal import Animal
+from project.animal import Animal
+
+
+class Reptile(Animal):
+    def __init__(self, name):
+        Animal.__init__(self, name)
+
