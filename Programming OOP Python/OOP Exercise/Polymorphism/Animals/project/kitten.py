3497ebf8681e98e3e1fc1857dfd5049a59bb932c
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-06-21 06:26:01 +0300

    Create kitten.py

---
+++ Programming OOP Python/OOP Exercise/Polymorphism/Animals/project/kitten.py
@@ -0,0 +1,14 @@
+# from Polymorphism.Animals.project.cat import Cat
+from project.cat import Cat
+
+
+class Kitten(Cat):
+
+    def __init__(self, name, age):
+        super().__init__(name, age, gender='Female')
+
+    def make_sound(self):
+        return 'Meow'
+
+
+
