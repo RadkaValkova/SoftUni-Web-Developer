2341b534f4dde5139bac8b493d2a2081b2bbd0b0
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-06-25 11:57:13 +0300

    Create salve.py

---
+++ Programming OOP Python/Exam_19122020/project/medicine/salve.py
@@ -0,0 +1,9 @@
+from project.medicine.medicine import Medicine
+
+
+class Salve(Medicine):
+    def __init__(self):
+        super().__init__(health_increase=50)
+
+    # def apply(self, survivor):
+    #     survivor.health += self.__health_increase
