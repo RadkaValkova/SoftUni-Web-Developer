a90851db86cf39da24ce022069b62812559ce38b
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-06-26 06:42:07 +0300

    Create survivor.py

---
+++ Programming OOP Python/Exam_prparation/project/survivor.py
@@ -0,0 +1,60 @@
+class Survivor:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        self.health = 100
+        self.needs = 100
+
+    @property
+    def needs_sustenance(self):
+        return self.needs < 100
+
+    @property
+    def needs_healing(self):
+        return self.health < 100
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value == '':
+            raise ValueError('Name not valid!')
+        self.__name = value
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        if value < 0:
+            raise ValueError('Age not valid!')
+        self.__age = value
+
+    @property
+    def health(self):
+        return self.__health
+
+    @health.setter
+    def health(self, value):
+        if value < 0:
+            raise ValueError('Health not valid!')
+        elif value >= 0 and value <= 100:
+            self.__health = value
+        else:
+            self.__health = 100
+
+    @property
+    def needs(self):
+        return self.__needs
+
+    @needs.setter
+    def needs(self, value):
+        if value < 0:
+            raise ValueError('Needs not valid!')
+        elif value >= 0 and value <= 100:
+            self.__needs = value
+        else:
+            self.__needs = 100
