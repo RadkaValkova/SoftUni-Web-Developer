61d3414373498bb6009ae70e8d17f26cd2d88ea5
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-06-29 22:39:19 +0300

    Update 2D_Rectangle_area.py

--- Programming Basics Python/01 Simple Calculation/2D_Rectangle_area.py
+++ Programming Basics Python/01 Simple Calculation/2D_Rectangle_area.py
@@ -19,3 +19,14 @@ area = a * b
 perimeter = 2 * (a+b)
 print('Area = ' + str(area))
 print('Perimeter = ' + str(perimeter))
+
+x1 = float(input())
+y1 = float(input())
+x2 = float(input())
+y2 = float(input())
+a = abs(x1-x2)
+b = abs(y1-y2)
+area = a * b
+perimeter = 2 * (a+b)
+print('Area = ' + str(area))
+print('Perimeter = ' + str(perimeter))
